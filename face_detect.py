48cfe57c69fbe2d3390abc1a03aa713901ad4712
RanksDG <53052392+RanksDG@users.noreply.github.com>
2023-01-26 22:32:11 -0500

    Add files via upload
    Completed face detection, picture taking, and face locating

--- face_detect.py
+++ face_detect.py
@@ -5,18 +5,16 @@ from tkinter.tix import ButtonBox
 import cv2 as cv
 import os
 import numpy as numpy 
+import time
 
 cap = cv.VideoCapture(0)  #this captures video from laptop camera
 
 img_counter = 0 
-location = (0,0)
-bottom_left =(10,10)
-top_left =(20,80)
-bottom_right = (80,20)
-top_right = (80,80)
-center = (50,50)
+
+quadrant = "none"
 
 while cap.isOpened():
+    new_quadrant = "none"
     ret,frame = cap.read() # reads the image one by one
     if not ret:
         print("Unable to take picture")
@@ -39,22 +37,39 @@ while cap.isOpened():
         photo = cv.imread(img_name)
         gray = cv.cvtColor(photo ,cv.COLOR_BGR2GRAY)
         #cv.imshow("Gray", gray)
-
        #Face Dectection
+
+        frameCopy = frame # unaltered copy of the frame
+        fheight , fwidth , fcolors = frame.shape #get frame width and height
+
         hair_face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
         face = hair_face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors =5)
         for x,y,w,h in face:
             cv.rectangle(photo, (x,y), (x+w, y+h), (255,0,0), 5)
+            #gets the x and y coordinates of the users face
+            xper = int(((x+w/2)/fwidth)*100)
+            yper = int(((y+h/2)/fheight)*100)
+            #Defines positons for coordinates
+            if xper < 50 and yper < 50:
+                new_quadrant = "topleft"
+            if xper >= 50 and yper < 50:
+                new_quadrant = "topright"
+            if xper < 50 and yper >= 50:
+                new_quadrant = "bottomleft"
+            if xper >= 50 and yper >= 50:
+                new_quadrant = "bottomright"
+    
+        #update the quadrant when it changes
+        if len(face) <= 1:
+            if(new_quadrant != quadrant):
+                quadrant = new_quadrant
+                print(quadrant)
+
+            #Shows picture taken
             cv.imshow("Face Detected", photo)
-            #t")
-
-            #if cv.rectangle(photo, (x,y), (x+w, y+h), (255,0,0), 5) ==
+           
         print("Faces Found", len(face))
-
-        #Next set center, left, right as certain pixels and create test to see if the face is there
-
 
 cap.release() #release video capture object
 cv.destroyAllWindows() # Closes all frame windowss
 
-#Gains access to webcam
