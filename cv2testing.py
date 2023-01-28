f738173498548dbb1b6d52e76a6088a9c135090b
Warvan1 <84527285+Warvan1@users.noreply.github.com>
2023-01-28 15:24:35 -0500

    Update cv2testing.py

--- cv2testing.py
+++ cv2testing.py
@@ -2,8 +2,20 @@
 import cv2 as cv2
 import os
 import numpy as numpy 
+import pyttsx3
+import threading
 
 print(cv2.__version__)
+
+engine = pyttsx3.init()
+
+#used to store the quadrant the face is detected in
+#global so that we can share it between sound and video thread
+global quadrant
+quadrant = "none"
+#used to kill the sound thread from the video thread
+global killAllThreads
+killAllThreads = False
 
 #determine the starting point for the image counter 
 #based on current files in the pictures folder
@@ -15,9 +27,6 @@ def starts_with(string):
 
 img_counter = len(list(filter(starts_with,os.listdir("pictures"))))
 
-#used to store the quadrant the face is detected in
-quadrant = "none"
-
 #face classifier data
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -28,52 +37,76 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #this captures video from laptop camera
 cap = cv2.VideoCapture(0)  
 
-while True:
-    new_quadrant = "none" #default is none till we find a face
+#video thread
+def videoLoop(face_cascade, img_counter):
+    global quadrant
+    global killAllThreads
+    while True:
+        new_quadrant = "none" #default is none till we find a face
 
-    ret,frame = cap.read() # reads the video input one frame at a time
-    frameCopy = frame # unaltered copy of the frame
-    fheight , fwidth , fcolors = frame.shape #get frame width and height
+        ret,frame = cap.read() # reads the video input one frame at a time
+        frameCopy = frame # unaltered copy of the frame
+        fheight , fwidth , fcolors = frame.shape #get frame width and height
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #converts frame to greyscale
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4) #detects faces on frame
-    for (x, y, w, h) in faces: 
-        #draw a rectangle around faces
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
-        #detect what quadrant the face is in
-        xper = int(((x+w/2)/fwidth)*100)
-        yper = int(((y+h/2)/fheight)*100)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #converts frame to greyscale
+        faces = face_cascade.detectMultiScale(gray, 1.1, 6) #detects faces on frame
+        for (x, y, w, h) in faces: 
+            #draw a rectangle around faces
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
+            #detect what quadrant the face is in
+            xper = int(((x+w/2)/fwidth)*100)
+            yper = int(((y+h/2)/fheight)*100)
+            
+            if xper < 50 and yper < 50:
+                new_quadrant = "top left"
+            if xper >= 50 and yper < 50:
+                new_quadrant = "top right"
+            if xper < 50 and yper >= 50:
+                new_quadrant = "bottom left"
+            if xper >= 50 and yper >= 50:
+                new_quadrant = "bottom right"
         
-        if xper < 50 and yper < 50:
-            new_quadrant = "topleft"
-        if xper >= 50 and yper < 50:
-            new_quadrant = "topright"
-        if xper < 50 and yper >= 50:
-            new_quadrant = "bottomleft"
-        if xper >= 50 and yper >= 50:
-            new_quadrant = "bottomright"
-    
-    #update the quadrant when it changes
-    if len(faces) <= 1:
-        if(new_quadrant != quadrant):
-            quadrant = new_quadrant
+        #update the quadrant when it changes
+        if len(faces) <= 1:
+            if(new_quadrant != quadrant):
+                quadrant = new_quadrant
+        
+        cv2.imshow("Picture", frame) #displays a frame
+
+        key = cv2.waitKey(1) #wait until this key is pressed
+        if key == ord("q"): # press q to exit this while loop
+            killAllThreads = True
+            break
+        if key == ord("p"):
+
+            # TODO: ### if user in correct quadrant and cheese voice command 
+            
+            img_name = "pictures/opencvframe{}.jpg".format(img_counter) #saves taken photo
+            cv2.imwrite(img_name, frameCopy) 
+            print("Photo Taken")
+            img_counter+=1
+
+#sound thread
+def soundLoop():
+    global quadrant
+    global killAllThreads
+    localQuadrant = "none"
+    while killAllThreads == False:
+        if(localQuadrant != quadrant):
+            localQuadrant = quadrant
             print(quadrant)
+            engine.say(quadrant)
+            engine.runAndWait()
 
-            #TODO: ### tell the user what quadrant we are in with playsound
-    
-    cv2.imshow("Picture", frame) #displays a frame
+#handle threading
+vthread = threading.Thread(target=videoLoop, args=[face_cascade, img_counter])
+sthread = threading.Thread(target=soundLoop)
 
-    key = cv2.waitKey(1) #wait until this key is pressed
-    if key == ord("q"): # press q to exit this while loop
-        break
-    if key == ord("p"):
+vthread.start()
+sthread.start()
 
-        # TODO: ### if user in correct quadrant and cheese voice command 
-        
-        img_name = "pictures/opencvframe{}.jpg".format(img_counter) #saves taken photo
-        cv2.imwrite(img_name, frameCopy) 
-        print("Photo Taken")
-        img_counter+=1
+vthread.join()
+sthread.join()
 
 cap.release() #release video capture object
 cv2.destroyAllWindows() # Closes all frame windows
